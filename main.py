bc94bc241485936ac0feaacde7d78b73c82d3306
Hemanth Vadde <140707363+hemanth-vadde-official@users.noreply.github.com>
2024-10-24 16:16:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def read_root():
     Returns:
         dict: A dictionary with a 'message' key and 'Hello, World!' as value.
     """
-    return {"Hello, World!"}
+    return {"message": "Hello, World!"}
